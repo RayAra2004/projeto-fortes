f872152c0a9ddcb3b4e653eff80c702503f0beea
Raynan Silva <raynan.araujo@gmail.com>
2024-05-29 23:51:22 -0300

    creating layout login (index)

---
+++ js/index.js
@@ -0,0 +1,7 @@
+document.getElementById("formLogin").addEventListener("submit", function(e){
+    e.preventDefault();
+});
+
+document.getElementById("signUp").addEventListener("click", function(e){
+    window.location = "./../pages/cadastro.html"
+});
