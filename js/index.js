16082dcd46b7195417ed09c24b8776345cc90d82
Raynan Silva <raynan.araujo@gmail.com>
2024-05-30 19:26:43 -0300

    creating page open notices

--- js/index.js
+++ js/index.js
@@ -1,5 +1,10 @@
 document.getElementById("formLogin").addEventListener("submit", function(e){
     e.preventDefault();
+    console.log(e)
+    if(e.target[0].value === "ong@gmail.com" && e.target[1].value === "adm123"){
+        window.location.href = "./pages/instituicao/index.html"
+    }
+    
 });
 
 document.getElementById("signUp").addEventListener("click", function(e){
